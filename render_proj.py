999f0f63b422b0a6518bf1fc8a0ad1ccd4823159
Alex Ong <alex.ong@unsw.edu.au>
2022-11-28 10:10:26 +1100

    new: Ability to render losslesscut projects

---
+++ render_proj.py
@@ -0,0 +1,34 @@
+import os
+import json5
+import ast
+import subprocess
+
+
+def process_file(file_name):
+    """processes a single llc file; making several clips"""
+    with open(file_name) as file:
+        data = json5.load(file)
+        video_file = data["mediaFileName"]
+        for index, item in enumerate(data["cutSegments"]):
+            process_clip(video_file, item, index)
+
+
+def process_clip(file_name, clip_info, index):
+    """Render a single clip"""
+    print(file_name, clip_info, index)
+    file_name_no_ext = os.path.splitext(file_name)[0]
+    start_str = f'-ss {clip_info["start"]}' if "start" in clip_info else ""
+    end_str = f'-to {clip_info["end"]}' if "end" in clip_info else ""
+    command = f'ffmpeg -hwaccel auto {start_str} {end_str} -i "{file_name}" -c:v h264_nvenc -cq:v 19 "{file_name_no_ext}-{index}.mp4"'
+    subprocess.run(command)
+
+
+def main():
+    """process everything in the current directory"""
+    for file in os.listdir("."):
+        if file.endswith("-proj.llc"):
+            process_file(file)
+
+
+if __name__ == "__main__":
+    main()
