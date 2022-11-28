106d92d1d005ece7d23b65071cb97417e226546a
Alex Ong <alex.ong@unsw.edu.au>
2022-11-28 14:14:25 +1100

    new: Put processed files into clips folder

--- render_proj.py
+++ render_proj.py
@@ -1,3 +1,6 @@
+"""
+Renders a folder of lossless-cut files (.llc) with associated videos
+"""
 import os
 import json5
 import ast
@@ -6,6 +9,7 @@ import subprocess
 
 def process_file(file_name):
     """processes a single llc file; making several clips"""
+    os.makedirs("clips", exist_ok=True)
     with open(file_name) as file:
         data = json5.load(file)
         video_file = data["mediaFileName"]
@@ -19,7 +23,7 @@ def process_clip(file_name, clip_info, index):
     file_name_no_ext = os.path.splitext(file_name)[0]
     start_str = f'-ss {clip_info["start"]}' if "start" in clip_info else ""
     end_str = f'-to {clip_info["end"]}' if "end" in clip_info else ""
-    command = f'ffmpeg -hwaccel auto {start_str} {end_str} -i "{file_name}" -c:v h264_nvenc -cq:v 19 "{file_name_no_ext}-{index}.mp4"'
+    command = f'ffmpeg -hwaccel auto {start_str} {end_str} -i "{file_name}" -c:v h264_nvenc -cq:v 19 "clips/{file_name_no_ext}-{index}.mp4"'
     subprocess.run(command)
 
 
