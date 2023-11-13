4eff152e24888eb7a50a7f8d83470e3d6056e0cc
alexo <the.onga@gmail.com>
2023-11-14 09:08:38 +1100

    feat: faster scene detection

--- autocrop.py
+++ autocrop.py
@@ -2,6 +2,16 @@ import os
 import subprocess
 import sys
 
+def process_video2():
+    os.makedirs("temp", exist_ok=True)
+    FIFTY_FPS = 'ffmpeg -skip_frame nokey -an -hwaccel auto -i output.mp4 -vsync 0 -vf "setpts=expr=N/50/TB,scale=1280:720" -c:v hevc_nvenc output_50fps.mp4'
+    subprocess.run(FIFTY_FPS, shell=True)
+    ONE_FPS = "ffmpeg -itsscale 50 -i output_50fps.mp4 -r 1 -an -c:v copy output_3fps.mp4"
+    subprocess.run(ONE_FPS, shell=True)
+    SCENE_DIFF = "ffmpeg -i output_3fps.mp4 -filter_complex \"select='gt(scene,0.3)',metadata=print:file=time.txt\" -vsync vfr temp/img%03d.png"
+    #print(scene_diff)
+    subprocess.run(SCENE_DIFF, shell=True)
+    
 def process_video():
     os.makedirs("temp", exist_ok=True)
     # 24 minutes for 2.25 hour video
@@ -35,7 +45,7 @@ if __name__ == "__main__":
     print(sys.argv)
     directory = sys.argv[1]
     os.chdir(directory)
-    process_video()
+    process_video2()
     output = process_text()
     with open("youtube.txt", "w", encoding="utf8") as file:
         file.writelines("\n".join(output))
